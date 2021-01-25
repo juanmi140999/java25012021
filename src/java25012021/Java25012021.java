b3815bab511d5d113536e2b4f7dc2d9ffc8b14b4
JuanMiguel <juanmiguel.saugar@campusfp.es>
2021-01-25 10:06:22 +0100

    2 snapshot main1.java array double

--- src/java25012021/Java25012021.java
+++ src/java25012021/Java25012021.java
@@ -6,12 +6,18 @@ public class Java25012021 {
 
     
     public static void main(String[] args) {
-        String[] nombre = {"Luis","Miguel","Carlos"}; //Declarar un array
+        String[] nombre = {"Luis","Miguel","Carlos"}; //Declarar un array tipo cadena
         //Recorer el array
         
         for(int i=0; i<nombre.length; i++){
             System.out.println(nombre[i]);
         }
+        
+        double[] estatura = {1.72,1.75,1.65};
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(estatura[i]);
+        }
+        
     }
     
 }
