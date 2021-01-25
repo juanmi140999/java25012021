5ea740709465e497d2ad7abfe7d704dd21fbdf8b
JuanMiguel <juanmiguel.saugar@campusfp.es>
2021-01-25 10:00:35 +0100

    1 snapshot main1.java

---
+++ src/java25012021/Java25012021.java
@@ -0,0 +1,17 @@
+
+package java25012021;
+
+
+public class Java25012021 {
+
+    
+    public static void main(String[] args) {
+        String[] nombre = {"Luis","Miguel","Carlos"}; //Declarar un array
+        //Recorer el array
+        
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);
+        }
+    }
+    
+}
